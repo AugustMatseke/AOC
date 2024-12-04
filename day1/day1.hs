e3ecb695e441f9bc1927bc29860a6aeded34c307
AugustMatseke <118399526+AugustMatseke@users.noreply.github.com>
2024-12-04 18:31:35 +0100

    Day1 complete

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,20 +1,32 @@
-module Main where
+module Day1 where
 import Data.List (sort)
+
+readi :: String -> Int
+readi = read
 
 main :: IO ()
 main = do
-    let x = compareLists [2,5,7] [1,2,3]
-    print x
-
-
-
+    contents <- readFile "data.txt"
+    let eachLine =  map words $ lines contents
+    let listOne  =  sort $ map (readi . head) eachLine
+    let listTwo  =  sort $ map (readi . last) eachLine 
+    
+    let diff     = compareLists listOne listTwo
+    let similar  = similarityScore listOne listTwo
+    
+    print $ show diff ++ ", " ++ show similar
+    
+    
+    
 
 compareLists :: [Int] -> [Int] -> Int
-compareLists (x:xs) (y:ys) 
-    | length xs /= length ys = error "Lists are wrong Length"
-    | otherwise = comparedLists (sort xs) (sort ys)
-    where
-        comparedLists [] [] = 0
-        comparedLists (x:xs) (y:ys) = abs(x-y) + comparedLists xs ys
-        comparedLists _ _ = 0
+compareLists (x:xs) (y:ys) = abs(x - y) + compareLists xs ys
+compareLists _ _ = 0
+ 
+similarityScore :: [Int] -> [Int] -> Int
+similarityScore (x:xs) rest = (x * count x rest)  + similarityScore xs rest
+similarityScore _ _ = 0
+
+count   :: Eq a => a -> [a] -> Int
+count x =  length . filter (==x)
     
