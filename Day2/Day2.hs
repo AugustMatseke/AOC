ef5790da5cc2cee2a1dc8ac6af930cc30a875472
AugustMatseke <118399526+AugustMatseke@users.noreply.github.com>
2024-12-18 02:00:14 +0100

    Day2 start

--- Day2/Day2.hs
+++ Day2/Day2.hs
@@ -1,18 +1,31 @@
 module Day2 where
-import Text.Read (Lexeme(String))
+-- import Text.Read (Lexeme(String))
 
 main :: IO ()
 main = do
     content <- readFile "data.txt"
+    let vals = parse content
+    let test = [1, 3, 8, 10]
 
-    let result = extractNums content
+    print $ diffs test
 
-    print result
+    -- print $ head vals
 
 
--- isSafe
+isSafe :: [Int] -> Bool
+isSafe x = monotone x && True
+    -- | all $ 
 
-extractNums :: String -> [[Int]]
-extractNums string = map (map read. words) (lines string)
---One line is all you ever need
+monotone :: [Int] -> Bool
+monotone xs  
+    | and $ zipWith (<) xs (tail xs) = True
+    | and $ zipWith (>) xs (tail xs) = True
+    | otherwise = False
 
+diffs ::  [Int] -> Bool
+diffs x =  do
+    let list = zipWith (-) (tail x) x
+    return True
+
+parse :: String -> [[Int]]
+parse string = map (map read. words) (lines string)
--- day1/day1.hs
+++ day1/day1.hs
@@ -6,19 +6,15 @@ import Data.List (sort)
 main :: IO ()
 main = do
     contents <- readFile "data.txt"
-    let listOne  =  sort $ map head $ extractNums contents
-    let listTwo  =  sort $ map last $ extractNums contents
+    let (listOne, listTwo) = extractNums contents
     
     let diff     = compareLists listOne listTwo
     let similar  = similarityScore listOne listTwo
     print $ show diff ++ ", " ++ show similar
-    
-
 
 compareLists :: [Int] -> [Int] -> Int
-compareLists (x:xs) (y:ys) = abs(x - y) + compareLists xs ys
-compareLists _ _ = 0
- 
+compareLists (x:xs) (y:ys) = abs (x - y) + compareLists xs ys
+
 similarityScore :: [Int] -> [Int] -> Int
 similarityScore (x:xs) rest = (x * count x rest)  + similarityScore xs rest
 similarityScore _ _ = 0
@@ -26,7 +22,8 @@ similarityScore _ _ = 0
 count   :: Eq a => a -> [a] -> Int
 count x =  length . filter (==x)
 
-extractNums :: String -> [[Int]]
-extractNums string = map (map read . words) (lines string)
+extractNums :: String -> ([Int], [Int])
+extractNums string =
+    let (s1, s2) = unzip $ map ((\[x, y] -> (read x, read y)) . words) (lines string)
+    in (s1, s2)
 --One line is all you need
-    
