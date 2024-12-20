264a29a45b4667ca8727bde61d2f6f3e7514953c
AugustMatseke <118399526+AugustMatseke@users.noreply.github.com>
2024-12-20 23:11:53 +0100

    day2

--- Day2/Day2.hs
+++ Day2/Day2.hs
@@ -6,21 +6,37 @@ main = do
     let vals = parse content
 
     print $ part1 vals
+    print $ part2 vals
 
 
 part1 :: [[Int]] -> Int
-part1 = length . filter isSafe
+part1 vals = length $ filter isSafe vals
+
+part2 :: [[Int]] -> Int
+part2 [] = 0
+part2 (x:xs) = case (monotone x, diffs x) of
+    (0, 0) -> 1 + part2 xs
+    _      -> if dampened x then 1 + part2 xs else part2 xs
+    where
+        dampened :: [Int] -> Bool
+        dampened x = any isSafe (brute x)
+
+brute :: [a] -> [[a]]
+brute [] = []
+brute (x:xs) = xs : map (x :) (brute xs)
 
 isSafe :: [Int] -> Bool
-isSafe x = monotone x && diffs x
+isSafe x = monotone x == 0 && diffs x == 0
 
-monotone :: [Int] -> Bool
-monotone xs = all (uncurry (<)) pairs || all (uncurry (>)) pairs
+monotone :: [Int] -> Int
+monotone xs = min (length (filter not lt)) (length (filter not gt))
     where pairs = zip xs (tail xs)
+          lt = map (uncurry (<)) pairs
+          gt = map (uncurry (>)) pairs
 
-diffs :: [Int] -> Bool
-diffs nums = all (\x -> x > 0 && x <= 3) $ zipWith (\a b -> abs (a - b)) nums (tail nums)
+diffs :: [Int] -> Int
+diffs nums = length $ filter (\(x, y) -> abs (x - y) > 3) pairs
+    where pairs = zip nums (tail nums)
 
 parse :: String -> [[Int]]
 parse string = map (map read. words) (lines string)
-
